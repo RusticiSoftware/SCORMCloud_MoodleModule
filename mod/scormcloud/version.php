076413c8136c2b53c27ea3d421e83ec86e8ca14f
Stuart Childs <stuart.childs@scorm.com>
2011-08-25 11:42:20 -0700

    Updated version.

--- mod/scormcloud/version.php
+++ mod/scormcloud/version.php
@@ -34,6 +34,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2011081700;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011082500;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010031900;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
