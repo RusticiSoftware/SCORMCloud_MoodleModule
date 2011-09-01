8983e391560edca52ba95ed7dc52d7c8342b3b72
Stuart Childs <stuart.childs@scorm.com>
2011-09-01 07:32:31 -0700

    Couple of changes to follow proper frankenstyle naming.

--- mod/scormcloud/db/access.php
+++ mod/scormcloud/db/access.php
@@ -1,19 +1,25 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ *   Copyright 2011 Rustici Software.
+ *
+ *   This file is part of the SCORM Cloud Module for Moodle.
+ *   https://github.com/RusticiSoftware/SCORMCloud_MoodleModule
+ *   http://scorm.com/moodle/
+ *
+ *   The SCORM Cloud Module is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The SCORM Cloud Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with the SCORM Cloud Module.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 /**
@@ -42,7 +48,7 @@
  * The variable name for the capability definitions array is $capabilities
  *
  * @package   mod_scormcloud
- * @copyright 2010 Your Name
+ * @copyright 2011 Rustici Software
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
