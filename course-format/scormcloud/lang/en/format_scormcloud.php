b00edce120fd4eb842672d5fe68e3553e0ebd907
Stuart Childs <stuart.childs@scorm.com>
2011-09-02 08:37:55 -0700

    Updated version.

--- course-format/scormcloud/lang/en/format_scormcloud.php
+++ course-format/scormcloud/lang/en/format_scormcloud.php
@@ -1,25 +1,31 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ *   Copyright 2011 Rustici Software.
+ *
+ *   This file is part of the SCORM Cloud Module for Moodle.
+ *   https://github.com/RusticiSoftware/SCORMCloud_MoodleModule
+ *   http://scorm.com/moodle/
+ *
+ *   The SCORM Cloud Module is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The SCORM Cloud Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with the SCORM Cloud Module.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
- * Strings for component 'format_scorm', language 'en', branch 'MOODLE_20_STABLE'
+ * Strings for component 'format_scormcloud', language 'en'
  *
- * @package   format_scorm
- * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @package   format_scormcloud
+ * @copyright 2011 Rustici Software
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
