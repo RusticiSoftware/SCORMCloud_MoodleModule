d5c3dbc311b3f2494db01efc8733aad42a8ef6d9
Stuart Childs <stuart.childs@scorm.com>
2011-09-16 11:46:44 -0500

    Update version.
    Also added simple packager script.

--- mod/scormcloud/version.php
+++ mod/scormcloud/version.php
@@ -34,6 +34,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2011090201;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011091600;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010031900;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
