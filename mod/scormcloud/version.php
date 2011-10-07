af67a7dfdf5b69d0807c53f839cc2ffdadea0357
Stuart Childs <stuart.childs@scorm.com>
2011-10-07 15:08:27 -0500

    Fix for plugin submission to Moodle database.
    version.php previously referenced a release of Moodle prior to 2.0 official release date.

--- mod/scormcloud/version.php
+++ mod/scormcloud/version.php
@@ -35,5 +35,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $module->version  = 2011100700;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010031900;  // Requires this Moodle version
+$module->requires = 2010112400;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
